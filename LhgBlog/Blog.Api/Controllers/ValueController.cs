f4bb2559111e93418162454a279895402cd3e7dc
李海国 <814385644@qq.com>
2018-08-25 18:33:48 +0800

    配置使用AddHttpsRedirection

---
+++ LhgBlog/Blog.Api/Controllers/ValueController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Api.Controllers
+{
+    [Route("api/values")]
+    public class ValueController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+    }
+}
--- LhgBlog/Blog.Api/Startup.cs
+++ LhgBlog/Blog.Api/Startup.cs
@@ -11,12 +11,36 @@ namespace Blog.Api
 {
     public class Startup
     {
+        //服务注册
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            //注册Https Service
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect; //状态码
+                options.HttpsPort = 5001; //使用的端口
+            });
+            //注册hSTS
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+                options.ExcludedHosts.Add("example.com");
+                options.ExcludedHosts.Add("www.example.com");
+            });
+
+
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app">用于构建中间件管道</param>
+        /// <param name="env">环境变量</param>
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -25,10 +49,22 @@ namespace Blog.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //使用Hsts
+            app.UseHsts();
+            //使用Https 重定向中间件（注意，这个使用中间件的顺序很重要，必须要在UseMvc之前）
+            app.UseHttpsRedirection();
+
+
+
+
+
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            //配置使用MVC中间件（默认的路由，特性方式）
+            app.UseMvc();
         }
     }
 }
